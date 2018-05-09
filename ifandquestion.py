e7f0fda5d42294a2cb8d97efffa0f6488a5e2723
missa-gray <39106904+missa-gray@users.noreply.github.com>
2018-05-08 21:07:54 -0400

    Update ifandquestion.py

--- ifandquestion.py
+++ ifandquestion.py
@@ -1,11 +1,11 @@
 def rental_car_cost (days)
 cost = days * 40
-if days = 7:
+if days > 7:
   then cost -= 50
   """This or.."""
-elif 3 <= days <=7:
+elif 3 <= days <=6:
   cost -= 20
   """This?"""
-elif days >=3 and days <=7:
+elif days >=3 and days <=6:
   cost -= 20
 return cost
