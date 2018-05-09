ff676ab92e310f2e1be93b7e0512a342d9b9d105
missa-gray <39106904+missa-gray@users.noreply.github.com>
2018-05-08 21:05:40 -0400

    Create ifandquestion.py

---
+++ ifandquestion.py
@@ -0,0 +1,11 @@
+def rental_car_cost (days)
+cost = days * 40
+if days = 7:
+  then cost -= 50
+  """This or.."""
+elif 3 <= days <=7:
+  cost -= 20
+  """This?"""
+elif days >=3 and days <=7:
+  cost -= 20
+return cost
